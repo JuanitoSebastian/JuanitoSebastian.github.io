e1c6134443074e564241d7970160ae936472ecb0
Juan Covarrubias <me@juan.fi>
2022-11-08 16:45:38 +0200

    Added tailwind to project

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-	},
+        extend: {
+          colors: {
+            'j-light-lilac': '#F0F0FC',
+            'j-lilac': '#CB9CF2',
+            'j-dark-lilac': '#540D6E',
+            'j-black': '#23201F'
+          },
+          fontFamily: {
+            'roc-grotesk': ['roc-grotesk', 'sans-serif']
+          }
+        }
+      },
 	plugins: [],
 }
