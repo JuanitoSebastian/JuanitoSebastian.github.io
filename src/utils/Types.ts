6864c6b7541577ec0536c657ddb825db56ba1102
Juan Covarrubias <me@juan.fi>
2022-11-12 17:35:20 +0200

    Work on posts

--- src/utils/Types.ts
+++ src/utils/Types.ts
@@ -3,8 +3,8 @@ export interface Frontmatter {
     description: string;
     author: string;
     date: string;
-    url: string;
-  }
+    tags: [string];
+}
 
 export interface Post {
     url: string;
