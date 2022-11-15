bb8990e7a7f827ac939970a9d7819a79086e1198
Juan Covarrubias <me@juan.fi>
2022-11-15 07:45:44 +0200

    Work on responsive header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,26 +9,39 @@ const Header = () => {
         return classes.filter(Boolean).join(' ')
     }
 
+    const headerHeight = () => {
+        const offset = 144 - scrollPosition;
+        return offset < 64 ? 64 : offset;
+    };
+
+    const navMargin = () => {
+        const offset = 40 - scrollPosition;
+        return offset < 8 ? 8 : offset;
+    };
+
+    const headerWrapperStyle = { height: `${headerHeight()}px` };
+    const navStyle = { marginTop: `${navMargin()}px` };
+
     return (
-        <div className={classNames(scrollPosition > 0 ? 'h-16' : 'h-36', 'w-full bg-white bg-opacity-80 fixed top-0 backdrop-blur z-20 transition items-center overflow-hidden')}>
-            <header className="container mx-auto max-w-6xl py-4 px-4">
-                <div className="grid grid-cols-2">
-                    <div className="flex flex-col gap items-start">
-                        <a href="/">
-                            <div className="flex flex-row">
-                                <h1 className="">Juan</h1>
-                                <h1 className={classNames(scrollPosition > 0 ? 'visible' : 'invisible')}>.fi</h1>
-                            </div>
-                            <h1 className={classNames(scrollPosition > 0 ? 'invisible' : 'visible')}>Sebastian</h1>
-                            <h1 className={classNames(scrollPosition > 0 ? 'invisible' : 'visible')}>Covarrubias</h1>
-                        </a>
+        <div style={headerWrapperStyle} className='w-full bg-white bg-opacity-80 fixed top-0 backdrop-blur z-20 transition items-center overflow-hidden shadow-lg'>
+            <header className="container mx-auto max-w-6xl px-4 pt-4">
+                    <div className="grid grid-cols-2 w-full h-full items-start">
+                        <div className="flex flex-col gap items-start">
+                            <a href="/">
+                                <div className="flex flex-row">
+                                    <h1 className="">Juan</h1>
+                                    <h1 className={classNames(scrollPosition > 0 ? 'visible' : 'invisible')}>.fi</h1>
+                                </div>
+                                <h1 className={classNames(scrollPosition > 0 ? 'invisible' : 'visible')}>Sebastian</h1>
+                                <h1 className={classNames(scrollPosition > 0 ? 'invisible' : 'visible')}>Covarrubias</h1>
+                            </a>
+                        </div>
+                        <nav style={navStyle} className="flex-row gap-8 items-center justify-end md:flex hidden">
+                            <a className="font-light" href="/#about-me">about me</a>
+                            <a className="font-light" href="/#my-work">my work</a>
+                            <a className="font-light" href="/#footer">contact</a>
+                        </nav>
                     </div>
-                    <nav className="flex-row gap-8 items-center justify-end md:flex hidden">
-                        <a className="font-light" href="/#about-me">about me</a>
-                        <a className="font-light" href="/#my-work">my work</a>
-                        <a className="font-light" href="/#footer">contact</a>
-                    </nav>
-                </div>
             </header>
         </div>
     );
