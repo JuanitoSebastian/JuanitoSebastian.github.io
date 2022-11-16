10daebd882ecdcf3135ce7617af3824127f52453
Juan Covarrubias <me@juan.fi>
2022-11-16 08:00:42 +0200

    Smoother header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,47 +4,43 @@ import { useScrollPosition } from '../hooks/useScrollPosition';
 const Header = () => {
 
     const scrollPosition = useScrollPosition();
+    const scrollCutOff = 80;
 
-    const classNames = (...classes: [string]) => {
-        return classes.filter(Boolean).join(' ')
+    const isHeaderCollapsed = () => {
+        return scrollPosition > scrollCutOff;
+    };
+
+    const headerTitle = () => {
+        const title = ["Juan.fi"];
+        const fullTitle = ["Juan", "Sebastian", "Covarrubias"]
+        return isHeaderCollapsed() ? title : fullTitle;
     }
 
     const headerHeight = () => {
-        return scrollPosition > 144 ? 64 : 144;
-    };
-
-    const navMargin = () => {
-        return scrollPosition > 144 ? 8 : 40;
+        return isHeaderCollapsed() ? 64 : 144;
     };
 
     const headerWrapperStyle = { height: `${headerHeight()}px` };
-    const navStyle = { marginTop: `${navMargin()}px` };
 
     return (
         <div style={headerWrapperStyle} className='w-full bg-white bg-opacity-80 fixed top-0 backdrop-blur z-20 transition-all duration-500 items-center overflow-hidden'>
-            <header className="container mx-auto max-w-6xl px-4 pt-4">
-                    <div className="grid grid-cols-2 w-full h-full items-start">
-                        <div className="flex flex-col gap items-start">
-                            <a href="/">
-                                <div className="flex flex-row">
-                                    <h1 className="">Juan</h1>
-                                    <h1 className={classNames(scrollPosition > 144 ? 'visible' : 'invisible')}>.fi</h1>
-                                </div>
-                                <h1 className={classNames(scrollPosition > 144 ? 'invisible' : 'visible')}>Sebastian</h1>
-                                <h1 className={classNames(scrollPosition > 144 ? 'invisible' : 'visible')}>Covarrubias</h1>
-                            </a>
+            <header className="container mx-auto max-w-6xl px-4 pt-4 h-full">
+                <div className="flex-row gap-8 items-center justify-end h-full flex transition-all duration-500">
+                    <a href="/">
+                        <div className="flex flex-col items-start justify-center">
+                            {headerTitle().map((text) => (
+                                <h1 key={text}>{text}</h1>
+                            ))}
                         </div>
-                        <nav style={navStyle} className="flex-row gap-8 items-center justify-end md:flex hidden transition-all duration-500">
-                            <a className="font-light" href="/#about-me">about me</a>
-                            <a className="font-light" href="/#my-work">my work</a>
-                            <a className="font-light" href="/#footer">contact</a>
-                        </nav>
-                    </div>
+                    </a>
+                    <div className="flex-grow"></div>
+                    <a className="font-light hidden md:block" href="/#about-me">about me</a>
+                    <a className="font-light hidden md:block" href="/#my-work">my work</a>
+                    <a className="font-light hidden md:block" href="/#footer">contact</a>
+                </div>
             </header>
         </div>
     );
 };
 
 export default Header;
-
-// w-full bg-white bg-opacity-50 fixed top-0 backdrop-blur z-20 h-36
