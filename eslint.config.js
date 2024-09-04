6427e87c150ece9e8ff10752be6861f9d99f45c4
Juan Covarrubias <me@juan.fi>
2024-09-04 06:38:01 +0300

    Fix eslint

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ import tseslint from 'typescript-eslint';
 
 export default [
   { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { ignores: ['src/env.d.ts'] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,2 @@
-import '../.astro/types';
+/// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
