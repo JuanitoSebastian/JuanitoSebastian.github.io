1dab867877bff0203233f7d6e751e08d5fed9682
Juan Covarrubias <me@juan.fi>
2025-01-25 12:27:31 +0200

    Remove unused npm packets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import react from '@astrojs/react';
-
-// https://astro.build/config
 import mdx from '@astrojs/mdx';
 import externalAnchorPlugin from './src/utils/externalAnchorPlugin';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx()],
+  integrations: [mdx()],
   site: 'https://JuanitoSebastian.github.io',
   markdown: {
     remarkPlugins: [externalAnchorPlugin],
