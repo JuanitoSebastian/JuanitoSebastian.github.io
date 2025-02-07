96dcd5dc7371c2d2ed78c3b72afa82c4edbdb58e
Juan Covarrubias <me@juan.fi>
2025-02-07 11:34:08 +0200

    Install and implement sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 import mdx from '@astrojs/mdx';
 import externalAnchorPlugin from './src/utils/externalAnchorPlugin';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
-  site: 'https://JuanitoSebastian.github.io',
+  integrations: [mdx(), sitemap()],
+  site: 'https://juan.fi',
   markdown: {
     remarkPlugins: [externalAnchorPlugin],
   },
