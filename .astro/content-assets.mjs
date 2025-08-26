13cbe0e04a63908ad82d761a4e8530e6df1d2f81
Juan Covarrubias <me@juan.fi>
2025-08-26 20:35:06 +0300

    Delete blog

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,4 +1,1 @@
-
-import __ASTRO_IMAGE_IMPORT_Z1KKgWt from "./imgs/udp_turntables.webp?astroContentImageFlag=&importer=content%2Fblog%2Fupd-sockets-and-digital-turntables.mdx";
-export default new Map([["./imgs/udp_turntables.webp?astroContentImageFlag=&importer=content%2Fblog%2Fupd-sockets-and-digital-turntables.mdx", __ASTRO_IMAGE_IMPORT_Z1KKgWt]]);
-		
+export default new Map();
--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,4 +1,1 @@
-
-export default new Map([
-["content/blog/upd-sockets-and-digital-turntables.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=content%2Fblog%2Fupd-sockets-and-digital-turntables.mdx&astroContentModuleFlag=true")]]);
-		
+export default new Map();
